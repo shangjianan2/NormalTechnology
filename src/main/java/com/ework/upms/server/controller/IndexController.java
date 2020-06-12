877dacb25d931af60e7770cbe6250f904a167ce2
wangjialun <wangjialun@jd.com>
2020-06-12 11:46:10 +0800

    origin

---
+++ src/main/java/com/ework/upms/server/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.ework.upms.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/hello")
+    public ModelAndView hello() {
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.addObject("name","zhangsan");
+        modelAndView.setViewName("hello");
+        return modelAndView;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+　　<h2>Hello World!</h2>
+　　<a href="${pageContext.request.contextPath}/hello">hello</a>
+</body>
+</html>
