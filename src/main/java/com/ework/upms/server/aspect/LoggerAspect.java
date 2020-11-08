a07cad720393e738117050bb680e999af25c6855
L <1978707987@qq.com>
2020-11-08 09:31:51 +0800

    添加日志切面

---
+++ src/main/java/com/ework/upms/server/aspect/LoggerAspect.java
@@ -0,0 +1,93 @@
+package com.ework.upms.server.aspect;
+
+import com.alibaba.fastjson.JSON;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Field;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description 打印请求参数和返回结果
+ * @Date 2019/2/26 11:37
+ * @Created by gongxz
+ */
+@Aspect
+@Configuration
+public class LoggerAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    // 定义切点Pointcut
+    @Pointcut("execution(* com.ework.upms.server.controller..*.*(..))")
+    public void executeService() {
+    }
+
+    @Around("executeService()")
+    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        HttpServletRequest request = sra.getRequest();
+
+        String url = request.getRequestURL().toString();
+        String method = request.getMethod();
+        //String uri = request.getRequestURI();
+        String queryString = request.getQueryString();
+        Object[] args = pjp.getArgs();
+        String params = "";
+        //获取请求参数集合并进行遍历拼接
+        if(args.length>0){
+            if("POST".equals(method)){
+                Object object = args[0];
+                Map map = getKeyAndValue(object);
+                params = JSON.toJSONString(map);
+            }else if("GET".equals(method)){
+                params = URLDecoder.decode(queryString,"UTF-8");
+            }
+        }
+
+        logger.info("请求开始===地址:"+url);
+        logger.info("请求开始===类型:"+method);
+        logger.info("请求开始===参数:"+params);
+
+        // result的值就是被拦截方法的返回值
+        Object result = pjp.proceed();
+        logger.info("请求结束===返回值:" + JSON.toJSON(result));
+        return result;
+    }
+
+    public static Map<String, Object> getKeyAndValue(Object obj) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        // 得到类对象
+        Class userCla = obj.getClass();
+        /* 得到类中的所有属性集合 */
+        Field[] fs = userCla.getDeclaredFields();
+        for (Field f : fs) {
+            f.setAccessible(true); // 设置些属性是可以访问的
+            Object val;
+            try {
+                val = f.get(obj);
+                // 得到此属性的值
+                map.put(f.getName(), val);// 设置键值
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        return map;
+    }
+
+
+
+}
