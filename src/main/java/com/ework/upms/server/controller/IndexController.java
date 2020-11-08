dee8b1821f68e539e81309b4f58d411ff2687ded
L <1978707987@qq.com>
2020-11-08 09:17:19 +0800

    添加日志

--- src/main/java/com/ework/upms/server/controller/IndexController.java
+++ src/main/java/com/ework/upms/server/controller/IndexController.java
@@ -1,12 +1,20 @@
 package com.ework.upms.server.controller;
 
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping("/hello")
     public ModelAndView hello() {
@@ -15,5 +23,12 @@ public class IndexController {
         modelAndView.setViewName("hello");
         System.out.println("controller");
         return modelAndView;
+    }
+
+    @RequestMapping("/display")
+    @ResponseBody
+    public String displayRequest() {
+        logger.info("test");
+        return "test";
     }
 }
